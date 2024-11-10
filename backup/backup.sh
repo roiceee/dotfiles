7a017c85b46bd6723fc750b8a81ba5d9e74d8645
John Roice Aldeza <jroicealdeza@gmail.com>
2024-11-11 03:36:02 +0800

    modify backup and restore scripts

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,2 +1,1 @@
-rsync -av --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/var/cache/*,/var/log/*,/swapfile} / /backup/destination
-
+sudo rsync -av --exclude={/dev,/proc,/sys,/tmp,/run,/mnt,/media,/var/cache,/var/log,/swapfile,/home/timeshift,swapfile-20gb} / /backup/destination
--- backup/restore.sh
+++ backup/restore.sh
@@ -1,1 +1,1 @@
-sudo rsync -av --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/var/cache/*,/var/log/*,/swapfile} /backup/destination/ /
+sudo rsync -av --exclude={/dev,/proc,/sys,/tmp,/run,/mnt,/media,/var/cache,/var/log,/swapfile,/home/timeshift,swapfile-20gb} /backup/destination/ /
