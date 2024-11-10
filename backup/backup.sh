95c4077aea1d568a2a42bd2e03482f5301960789
John Roice Aldeza <jroicealdeza@gmail.com>
2024-11-11 00:02:23 +0800

    add backup scripts

---
+++ backup/backup.sh
@@ -0,0 +1,2 @@
+rsync -av --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/var/cache/*,/var/log/*,/swapfile} / /backup/destination
+
---
+++ backup/restore.sh
@@ -0,0 +1,1 @@
+sudo rsync -av --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/var/cache/*,/var/log/*,/swapfile} /backup/destination/ /
